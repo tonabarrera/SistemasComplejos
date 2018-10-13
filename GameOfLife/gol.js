21efdfe972b4e8e8d807b26619b429e83c291ebc
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-10-13 09:57:03 -0500

    Ya pinta quien sabe porque

--- GameOfLife/gol.js
+++ GameOfLife/gol.js
@@ -1,12 +1,19 @@
 var c=document.getElementById("myCanvas");
-var longitud=630;
-var tam=50;
+var longitud=680;
+var tam=1000;
 var escala=longitud/tam;
 var ctx=c.getContext("2d");
+var random;
 for (var i = 0; i < tam; i++) {
 	for(var j=0;j<tam;j++){
-		ctx.rect(200+(j*(escala)),0+(i*(escala)),escala,escala);
+		random=Math.floor((Math.random() * 2) + 0);
+		if (random==1) {
+			ctx.fillStyle="#66ff66";
+		}else{
+			ctx.fillStyle="#FF0000";
+		}
+		ctx.fillRect(200+(j*(escala)),0+(i*(escala)),escala,escala);
 		ctx.stroke();
 	}
-	console.log("Ya hice: "+i);
+	// console.log("Ya hice: "+i);
 }
